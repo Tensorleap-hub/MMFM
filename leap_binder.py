5a0c50a36cc0c4a28b751770176cbda66e213fa7
tom Koren <tom.koren@tensorleap.ai>
2024-06-16 20:52:19 +0300

    WIP add input encoders

--- leap_binder.py
+++ leap_binder.py
@@ -1,40 +1,46 @@
 from typing import List, Union
 
 import numpy as np
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.datasets import mnist
-from tensorflow.keras.utils import to_categorical
 
 # Tensorleap imports
 from code_loader import leap_binder
 from code_loader.contract.datasetclasses import PreprocessResponse 
 from code_loader.contract.enums import Metric, DatasetMetadataType
 from code_loader.contract.visualizer_classes import LeapHorizontalBar
+from mmfm.dataset.dataset import Dictionary, IconQAFeatureDataset
+from mmfm.gcs_utils import _download
+from mmfm.config import cnf
+from mmfm.dataset.data_utils import init_dataset, crop_and_padding
+from PIL import Image
 
 # Preprocess Function
 def preprocess_func() -> List[PreprocessResponse]:
-    (train_X, train_Y), (val_X, val_Y) = mnist.load_data()
+    dict_path = init_dataset()
+    dictionary = Dictionary.load_from_file(dict_path)
+    eval_dset = IconQAFeatureDataset('test', 'choose_txt', 'resnet101_pool5_79_icon',
+                                     cnf.local_data_path, dictionary, 'bert-small', 34) # generate test data
+    dataset = PreprocessResponse(length=len(eval_dset), data={'dataset': eval_dset})
+    res = [dataset, dataset]
+    return res
 
-    train_X = np.expand_dims(train_X, axis=-1)  # Reshape :,28,28 -> :,28,28,1
-    train_X = train_X / 255                       # Normalize to [0,1]
-    train_Y = to_categorical(train_Y)           # Hot Vector
-    
-    val_X = np.expand_dims(val_X, axis=-1)  # Reshape :,28,28 -> :,28,28,1
-    val_X = val_X / 255                     # Normalize to [0,1]
-    val_Y = to_categorical(val_Y)           # Hot Vector
-
-    # Generate a PreprocessResponse for each data slice, to later be read by the encoders.
-    # The length of each data slice is provided, along with the data dictionary.
-    # In this example we pass `images` and `labels` that later are encoded into the inputs and outputs 
-    train = PreprocessResponse(length=len(train_X), data={'images': train_X, 'labels': train_Y})
-    val = PreprocessResponse(length=len(val_X), data={'images': val_X, 'labels': val_Y})
-    response = [train, val]
-    return response
 
 # Input encoder fetches the image with the index `idx` from the `images` array set in
 # the PreprocessResponse data. Returns a numpy array containing the sample's image. 
-def input_encoder(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
-    return preprocess.data['images'][idx].astype('float32')
+def img_encoder(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
+    img_path = preprocess.data['dataset'][idx]['img_path']
+    fp = _download(f'{cnf.cloud_dict}/{img_path}')
+    img = Image.open(fp)
+    const_img_size = img.resize(cnf.img_size)
+    padded_img = crop_and_padding(const_img_size)
+    return np.array(padded_img)
+
+
+def question_encoder(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
+    return preprocess.data['dataset'][idx]['question_token']
+
+
+def choice_encoder(idx: int, preprocess: PreprocessResponse) -> np.ndarray:
+    return preprocess.data['dataset'][idx]['choice_token'].swapaxes(0, 1)
 
 # Ground truth encoder fetches the label with the index `idx` from the `labels` array set in
 # the PreprocessResponse's data. Returns a numpy array containing a hot vector label correlated with the sample.
@@ -57,8 +63,8 @@ def bar_visualizer(data: np.ndarray) -> LeapHorizontalBar:
 LABELS = ['0','1','2','3','4','5','6','7','8','9']
 # Dataset binding functions to bind the functions above to the `Dataset Instance`.
 leap_binder.set_preprocess(function=preprocess_func)
-leap_binder.set_input(function=input_encoder, name='image')
+leap_binder.set_input(function=img_encoder, name='image')
 leap_binder.set_ground_truth(function=gt_encoder, name='classes')
-leap_binder.set_metadata(function=metadata_label, metadata_type=DatasetMetadataType.int, name='label')
-leap_binder.add_prediction(name='classes', labels=LABELS)
-leap_binder.set_visualizer(name='horizontal_bar_classes', function=bar_visualizer, visualizer_type=LeapHorizontalBar.type)
+# leap_binder.set_metadata(function=metadata_label, metadata_type=DatasetMetadataType.int, name='label')
+# leap_binder.add_prediction(name='classes', labels=LABELS)
+# leap_binder.set_visualizer(name='horizontal_bar_classes', function=bar_visualizer, visualizer_type=LeapHorizontalBar.type)
