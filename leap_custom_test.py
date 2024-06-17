87807e6a1a67064314c181706c18bfef73a2e476
tom Koren <tom.koren@tensorleap.ai>
2024-06-17 11:01:57 +0300

    add img vis

--- leap_custom_test.py
+++ leap_custom_test.py
@@ -1,6 +1,8 @@
 import os
-from leap_binder import preprocess_func, img_encoder, question_encoder, choice_encoder
+from leap_binder import preprocess_func, img_encoder, question_encoder, choice_encoder,\
+    gt_encoder, image_visualizer
 import tensorflow as tf
+import matplotlib.pyplot as plt
 # import numpy as np
 #
 # from leap_binder import preprocess_load, get_input_func, gt_encoder, metadata_is_truncated, metadata_length,\
@@ -21,12 +23,18 @@ def check_custom_integration():
     H5_MODEL_PATH = "model/end2end.h5"
     dir_path = os.path.dirname(os.path.abspath(__file__))
     model_path = os.path.join(dir_path, H5_MODEL_PATH)
-    model = tf.keras.models.load_model(model_path)
     x = preprocess_func()
+    loss = tf.keras.losses.CategoricalCrossentropy(from_logits=False)
     img = img_encoder(0, x[0])[None, ...]
+    vis_image = image_visualizer(img[0])
+    plt.imshow(vis_image.data)
     question = question_encoder(0, x[0])[None, ...]
     choices = choice_encoder(0, x[0])[None, ...]
+    gt = gt_encoder(0, x[0])[None, ...]
+    model = tf.keras.models.load_model(model_path)
     res = model([question, img, choices])
+    ls = loss(tf.nn.softmax(res), gt)
+    print(1)
 
 
 if __name__ == '__main__':
